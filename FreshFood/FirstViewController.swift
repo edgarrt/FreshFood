e5498a2b4dd6f60ae6668f7558108af52934fc25
edgarrt <edgar.trujillo@sjsu.edu>
2016-08-02 06:56:39 -0700

    Initial Commit

---
+++ FreshFood/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FreshFood
+//
+//  Created by Edgar Trujillo on 8/2/16.
+//  Copyright © 2016 Edgar Trujillo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
